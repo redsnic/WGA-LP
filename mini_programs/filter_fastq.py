cf6841f719f625a108a4f4cb274ab4d08d9dd674
Redsnic <nicolo@rossi.im>
2021-06-23 13:01:43 +0200

    added PE mode to filter-fastq

--- mini_programs/filter_fastq.py
+++ mini_programs/filter_fastq.py
@@ -14,6 +14,8 @@ from WGALP.blocks.fastq_bam_difference import filter_fastq_reads
 def prepare_input(args):
     input_data = InputManager("Select only listed reads from a Whole Genom Assembly")
     input_data.add_arg("--fastq", "path", description="fastq reads to be filtered (.fastq)")
+    input_data.add_arg("--fastq-fwd", "path", description="fastq reads to be filtered FWD (Paired end mode) (.fastq)")
+    input_data.add_arg("--fastq-rev", "path", description="fastq reads to be filtered REV (Paired end mode) (.fastq)")
     input_data.add_arg("--selected-reads", "path", description="a file containing the ids of the selected reads (each id is in its own line)")
     input_data.add_arg("--complement", "flag", description="if set, selected reads are instead removed from the original fastq")
     input_data.add_arg("--output", "dir", description="path to the output folder") 
@@ -22,8 +24,20 @@ def prepare_input(args):
 
 # --- input sanity checks
 
-def sanity_check(output_dir, fastq, selected_reads, complement):
-    check_files([fastq, selected_reads])
+def sanity_check(output_dir, fastq, fastq_fwd, fastq_rev, selected_reads, complement):
+    if fastq is not None and (fastq_fwd is not None and fastq_rev is not None):
+        raise Exception("Choose either PE [--fastq-rev and --fastq-fwd] or non PE [--fastq] mode, use --help for more information")
+
+    if fastq is None and not (fastq_fwd is not None and fastq_rev is not None):
+        raise Exception("For PE mode both fwd and rev fastq files must be specified, use --help for more information")
+
+    # files/dirs check
+    check_files(selected_reads)
+    if fastq is None:
+        check_files([fastq_fwd, fastq_rev])
+    else:
+        check_files([fastq])
+
     try:
         check_folders([output_dir])
     except Exception:
@@ -34,10 +48,15 @@ def sanity_check(output_dir, fastq, selected_reads, complement):
 
 # --- core functions
 
-def select_fastq_aux(output_dir, fastq, selected_reads, complement):
+def select_fastq_aux(output_dir, fastq, fastq_fwd, fastq_rev, selected_reads, complement):
     # sanity check
-    sanity_check(output_dir, fastq, selected_reads, complement)
-    filter_fastq_reads(fastq, selected_reads, os.path.join(output_dir, "filtered.fastq"), keep=not complement)
+    sanity_check(output_dir, fastq, fastq_fwd, fastq_rev, selected_reads, complement)
+    if fastq is None:
+        filter_fastq_reads(fastq_fwd, selected_reads, os.path.join(output_dir, "filtered_fwd.fastq"), keep=not complement)
+        filter_fastq_reads(fastq_rev, selected_reads, os.path.join(output_dir, "filtered_rev.fastq"), keep=not complement)
+    else:
+        filter_fastq_reads(fastq, selected_reads, os.path.join(output_dir, "filtered.fastq"), keep=not complement)
+    
      
 # --- caller function
 
@@ -45,11 +64,13 @@ def select_fastq(args):
     in_manager = prepare_input(args)
 
     fastq = in_manager["--fastq"]["value"]
+    fastq_fwd = in_manager["--fastq-fwd"]["value"]
+    fastq_rev = in_manager["--fastq-rev"]["value"]
     selected_reads = in_manager["--selected-reads"]["value"]
     output_dir = in_manager["--output"]["value"]
     complement = in_manager["--complement"]["value"]
     
-    select_fastq_aux(output_dir, fastq, selected_reads, complement)
+    select_fastq_aux(output_dir, fastq, fastq_fwd, fastq_rev, selected_reads, complement)
 
     print("task completed successfully")
     print("The filtered .fastq is at the following location:")
