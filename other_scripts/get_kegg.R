310ae129b005da10b233bc7022a7372587e5602b
Redsnic <nicolo@rossi.im>
2021-07-11 13:55:59 +0200

    added kegg extractor

---
+++ other_scripts/get_kegg.R
@@ -0,0 +1,13 @@
+# usage: Rscript get_kegg.R --args input.file > output.file
+library(tidyverse)
+args <- commandArgs(trailingOnly = TRUE)
+read_tsv(args[2], comment = "##") %>% 
+  select(KEGG_ko) %>%
+  mutate(KEGG_ko = ifelse(KEGG_ko == "-", NA, KEGG_ko)) %>%
+  pull(1) %>%
+  na.omit() %>%
+  map(~ strsplit(., ",")[[1]]) %>%
+  unlist() %>%
+  unique() %>% 
+  paste(collapse = "\n") %>% 
+  cat()
